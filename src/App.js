b5ccc7b891de364c4066dc9164308716604628a5
Eliot Du <108873919+EliotDu@users.noreply.github.com>
2022-10-24 16:18:35 +0800

    新增eslint、prettier、env設定

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -11,15 +11,15 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn google
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
